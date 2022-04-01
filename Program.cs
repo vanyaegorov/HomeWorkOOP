e5d346dfdf4413f06d18759ac0ec2f9de4360c53
Ivan <vanya-egorov@mail.ru>
2022-04-01 09:58:36 +0300

    01.04.2022

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lesson5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RationalNumbers x = new RationalNumbers(1, 2);
+            RationalNumbers y = new RationalNumbers(3, 4);
+            RationalNumbers sum = x + y;
+            RationalNumbers dif = x - y;
+            RationalNumbers mult = x * y;
+            RationalNumbers div = x / y;
+            Console.WriteLine(sum);
+            Console.WriteLine(dif);
+            Console.WriteLine(mult);
+            Console.WriteLine(div);
+            Console.WriteLine(x == y);
+            Console.WriteLine(x != y);
+            Console.WriteLine(x < y);
+            Console.WriteLine(x > y);
+            Console.WriteLine(x <= y);
+            Console.WriteLine(x >= y);
+            Console.WriteLine(x.Equals(y));
+        }
+    }
+}
---
+++ СomplexNumbers.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace lesson5
+{
+    class ComplexNumbers
+    {
+        int _a, _b, _i;
+        int Cn { get; set; }
+        public ComplexNumbers(int a, int b, int i)
+        {
+            _a = a;
+            _b = b;
+            _i = i;
+            Cn = a + b * i;
+        }
+        public ComplexNumbers(int cn)
+        {
+            cn = Cn;
+        }
+        public override string ToString()
+        {
+            if (Cn == 0)
+            {
+                Cn = _a + _b * _i;
+                string s = Convert.ToString(Cn);
+                return (s);
+            }
+            else
+            {
+                string s = Convert.ToString(Cn);
+                return (s);
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is ComplexNumbers numbers &&
+                   _a == numbers._a &&
+                   _b == numbers._b &&
+                   _i == numbers._i &&
+                   Cn == numbers.Cn;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_a, _b, _i, Cn);
+        }
+
+        public static ComplexNumbers operator +(ComplexNumbers cn1, ComplexNumbers cn2)
+        {
+            if (cn1._i != cn2._i)
+            {
+
+                throw new Exception();
+            }
+            else
+            {
+                int cn = (cn1._a + cn2._a) + (cn1._b + cn2._b) * cn1._i;
+                return new ComplexNumbers(cn);
+            }
+
+        }
+        public static ComplexNumbers operator -(ComplexNumbers cn1, ComplexNumbers cn2)
+        {
+            if (cn1._i != cn2._i)
+            {
+
+                throw new Exception();
+            }
+            else
+            {
+                int cn = (cn1._a - cn2._a) + (cn1._b - cn2._b) * cn1._i;
+                return new ComplexNumbers(cn);
+            }
+
+        }
+        public static ComplexNumbers operator *(ComplexNumbers cn1, ComplexNumbers cn2)
+        {
+            if (cn1._i != cn2._i)
+            {
+
+                throw new Exception();
+            }
+            else
+            {
+                int cn = (cn1._a*cn2._a-cn1._b-cn2._b)+(cn1._a*cn2._b+cn1._b*cn2._a)*cn1._i;
+                return new ComplexNumbers(cn);
+            }
+
+        }
+        public static ComplexNumbers operator /(ComplexNumbers cn1, ComplexNumbers cn2)
+        {
+            if (cn1._i != cn2._i)
+            {
+
+                throw new Exception();
+            }
+            else
+            {
+                int cn = (cn1._a + cn1._b * cn1._i) / (cn2._a + cn2._b * cn2._i);
+                return new ComplexNumbers(cn);
+            }
+
+        }
+        public static bool operator ==(ComplexNumbers cn1, ComplexNumbers cn2)
+        {
+            if (cn1.Cn == cn2.Cn || (cn1._a == cn2._a && cn1._b == cn2._b && cn1._i == cn2._i))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public static bool operator !=(ComplexNumbers cn1, ComplexNumbers cn2)
+        {
+            if (cn1.Cn != cn2.Cn || cn1._a != cn2._a || cn1._b != cn2._b || cn1._i != cn2._i)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
