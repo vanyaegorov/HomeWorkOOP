51c30d17b4b4fc0aabb61037f93fc2f9213449c4
Ivan <vanya-egorov@mail.ru>
2022-04-08 13:11:22 +0300

    08.04.2022

---
+++ Bcoder.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Bcoder : ICoder
+
+    {
+        public int[] uniArray=new int[32];
+        public int[] reversUniArray=new int[32];
+        public Bcoder()
+        {
+            uniArray[0] = 1040;
+            reversUniArray[0] = 1071;
+            for (int i = 1; i < uniArray.Length; i++)
+            {
+                uniArray[i] = uniArray[i - 1] + 1;
+                reversUniArray[i] = reversUniArray[i - 1] - 1;
+            }
+
+        }
+        public string Decode(string s)
+        {
+            int temp;
+            char[] symbols = s.ToCharArray();
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                temp = Convert.ToInt32(symbols[i]);
+                for (int j = 0; j < uniArray.Length; j++)
+                {
+                    if (uniArray[j] == temp)
+                    {
+                        temp = reversUniArray[j];
+                    }
+                }
+
+
+                symbols[i] = Convert.ToChar(temp);
+            }
+            return String.Concat(symbols);
+        }
+
+        public string Encode(string s)
+        {
+            int temp;
+            char[] symbols = s.ToCharArray();
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                temp = Convert.ToInt32(symbols[i]);
+                for (int j = 0; j < uniArray.Length; j++)
+                {
+                    if (reversUniArray[j] == temp)
+                    {
+                        temp = uniArray[j];
+                    }
+                }
+
+
+                symbols[i] = Convert.ToChar(temp);
+            }
+            return String.Concat(symbols);
+        }
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Acoder acoder = new Acoder();
+            string encoder = acoder.Encode("Привет МИР!");
+            Console.WriteLine(encoder);
+            Console.WriteLine(acoder.Decode(encoder));
+            Bcoder bcoder = new Bcoder();
+            string newencoder = bcoder.Encode("АБВГДЕ");
+            Console.WriteLine(newencoder);
+            Console.WriteLine(bcoder.Decode(newencoder));
+
+
+
+        }
+    }
+}
